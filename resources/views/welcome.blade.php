cc4d4a14cb1a8e32657dbda60195373f2b42ec89
YosukeWakayama <yuskrank@gmail.com>
2019-12-27 19:15:10 +0900

    kiiro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,7 +66,7 @@
     </style>
 </head>
 
-<body>
+<body style="background-color:yellow;">
     <div class="flex-center position-ref full-height">
         @if (Route::has('login'))
         <div class="top-right links">
