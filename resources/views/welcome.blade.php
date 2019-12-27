57616aac33b5cc38564c7d7e4414aec4f3e7787b
shinohara-san <someiyoshino0809@gmail.com>
2019-12-27 18:51:01 +0900

    shinotest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelがんばるぞ
+                    チーム開発がんばるぞ
                 </div>
 
                 <div class="links">
