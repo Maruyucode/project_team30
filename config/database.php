51da808b82f7b7d795bd3362150087909dd600bb
YosukeWakayama <yuskrank@gmail.com>
2019-12-28 15:42:47 +0900

    チャット機能実装のため、config/database.phpのredisの部分を書き換え

--- config/database.php
+++ config/database.php
@@ -117,13 +117,23 @@ return [
     |
     */
 
-    'redis' => [
+    // 'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+    //     'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+    //     'options' => [
+    //         'cluster' => env('REDIS_CLUSTER', 'redis'),
+    //         'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+    //     ],
+
+        'redis' => [
+            'client' => 'predis',
+            'default' => [
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 0,
+            ],
         ],
 
         'default' => [
